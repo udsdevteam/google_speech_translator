3cb2bf4de3c38f943cef5dc4b362dfe012068fb3
junho.nam <nowonlyhere@gmail.com>
2018-08-27 17:12:34 +0900

    first commit

---
+++ long-speech.js
@@ -0,0 +1,52 @@
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech');
+
+// Creates a client
+const client = new speech.SpeechClient();
+
+ const gcsUri = 'gs://voice-translator-speech/test.raw';
+ const encoding = 'LINEAR16';
+ const sampleRateHertz = 16000;
+ const languageCode = 'ko-KR';
+
+ const fs = require('fs');
+
+const config = {
+  encoding: encoding,
+  sampleRateHertz: sampleRateHertz,
+  languageCode: languageCode,
+};
+
+const audio = {
+  uri: gcsUri,
+};
+
+const request = {
+  config: config,
+  audio: audio,
+};
+
+// Detects speech in the audio file. This creates a recognition job that you
+// can wait for now, or get its result later.
+client
+  .longRunningRecognize(request)
+  .then(data => {
+    const operation = data[0];
+    // Get a Promise representation of the final result of the job
+    return operation.promise();
+  })
+  .then(data => {
+    const response = data[0];
+    const transcription = response.results
+      .map(result => result.alternatives[0].transcript)
+      .join('\n');
+    console.log(`Transcription: ${transcription}`);
+
+    fs.writeFile('result.txt', transcription, function (err) {
+  	if (err) throw err;
+	console.log('Saved!');
+    });
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
